b23d75dc50e684762fc0a3d84c7c28ac7b81c09d
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-04-20 21:06:03 +0630

    Test : Added Unit Test for App Component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {shallow, ShallowWrapper} from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
+import GitUser from './GitUser/GitUser';
 import {App} from './App';
 describe('App', () => {
   let subject: ShallowWrapper;
+
   beforeEach(() =>{
     subject = shallow(<App />);
   });
@@ -10,4 +12,15 @@ describe('App', () => {
   it('should be div compnent',() => {
     expect(subject.type()).toBe('div');
   });
+
+  it('should has App class',() =>{
+    expect(subject.hasClass('App')).toBe(true);
+  });
+
+  describe('GitUser component',() =>{
+    it('should has GitUser Component',() =>{
+      expect(subject.childAt(0).type()).toBe(GitUser);
+    });
+  });
+
 });
