cdabeb72668b174c194f127b7d1bfbc1d84baa4f
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-04-21 22:12:25 +0630

    Set up mock user for better testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -7,3 +7,10 @@ export default Enzyme;
 
 
 export const getDispatchMock = () => jest.fn().mockImplementation((dispatched) => dispatched);
+
+export const mockUser = {
+    login: 'mock login',
+    id: 123,
+    avatar_url: 'mock avatar url',
+    url: 'mock url',
+}
