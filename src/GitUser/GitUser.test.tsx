d208df34d81298dc1fd71a3fd42f0041390b018a
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-04-25 15:54:19 +0630

    Fixed Unit Test of GitUser

--- src/GitUser/GitUser.test.tsx
+++ src/GitUser/GitUser.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
+import AppHeader from './AppHeader/AppHeader';
 import SearchInput from './SearchInput/SearchInput';
 import UserResultList from './UserResultList/UserResultList';
 import ConnectedGitUser,{ GitUser } from './GitUser';
@@ -7,6 +8,7 @@ describe('Git User', () => {
   let subject: ShallowWrapper;
   const defaultProp = {
     searchText: 'Example SearchText',
+    noOfApiCall: 0,
     users: [
         {
             login: 'Example Login',
@@ -16,7 +18,8 @@ describe('Git User', () => {
         }
     ],
     actions: {
-        updateSearchTextService: jest.fn()
+        updateSearchTextService: jest.fn(),
+        callGitAPIService: jest.fn(),
     }
   };
 
@@ -32,27 +35,33 @@ describe('Git User', () => {
     expect(subject.hasClass('Git__User__Container')).toBe(true);
   });
 
+  describe('AppHeader component',() =>{
+    it('should be AppHeader component',() =>{
+      expect(subject.childAt(0).type()).toBe(AppHeader);
+    });
+  });
+  
   describe('SearchInput component',() =>{
     it('should has SearchInput',() =>{
-      expect(subject.childAt(0).type()).toBe(SearchInput);
+      expect(subject.childAt(1).type()).toBe(SearchInput);
     });
 
     it('should has searchText prop',() =>{
-        expect(subject.childAt(0).prop('searchText')).toBe(defaultProp.searchText);
+        expect(subject.childAt(1).prop('searchText')).toBe(defaultProp.searchText);
     });
 
     it('should has onSearchInputChange prop',() =>{
-        expect(subject.childAt(0).prop('onSearchInputChange')).toBe(defaultProp.actions.updateSearchTextService);
+        expect(subject.childAt(1).prop('onSearchInputChange')).toBe(defaultProp.actions.updateSearchTextService);
     });
   });
 
   describe('UserResultList component',() =>{
     it('should has UserResultList',() =>{
-        expect(subject.childAt(1).type()).toBe(UserResultList);
+        expect(subject.childAt(2).type()).toBe(UserResultList);
     });
 
     it('should has users prop',() =>{
-        expect(subject.childAt(1).prop('users')).toBe(defaultProp.users);
+        expect(subject.childAt(2).prop('users')).toBe(defaultProp.users);
     });
   });
 
