29bc286bf07e1a74e731de6d783e0d7b218bdbb6
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-04-26 10:23:09 +0630

    Update Unit Test of GitUserService

--- src/GitUser/GitUserService.test.ts
+++ src/GitUser/GitUserService.test.ts
@@ -1,27 +1,39 @@
-import { updateSearchTextService } from './GitUserService';
-import { updateSearchText,updateUsers } from './GitUser.dispatch';
+import { updateSearchTextService,callGitAPIService } from './GitUserService';
+import { updateSearchText,updateUsers,updateNoOfApiCall } from './GitUser.dispatch';
 import { getGitUsers } from './getGitUser';
-import { getDispatchMock } from '../setupTests';
+import { getDispatchMock,mockUser } from '../setupTests';
+import { AppState } from '../store';
 jest.mock('./getGitUser');
 jest.mock('./GitUser.dispatch');
 describe('Git Users Service', () => {
   const searchText = 'Search';
+  const mockNoOfApiCall = 2;
   let dispatchMock: jest.Mock;
-  const git_users = [
-    {
-        login: 'login',
-        id: 123,
-        avatar_url: 'avatar url',
-        url: 'url'
-    }
-];
+  let getState: () => AppState;
   beforeEach(() => {
     dispatchMock = getDispatchMock();
-    (getGitUsers as jest.Mock).mockReturnValue(git_users);
+    getState = jest.fn().mockReturnValue({
+      git_user: {noOfApiCall: mockNoOfApiCall}
+    });
+    (getGitUsers as jest.Mock).mockReturnValue([mockUser])
     updateSearchTextService(searchText)(dispatchMock);
+    callGitAPIService(searchText)(dispatchMock,getState);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should call updateSearchText',() =>{
     expect(updateSearchText).toHaveBeenCalledWith(dispatchMock,searchText);
   });
+
+  it('should call update user with mockuser array',() => {
+    expect(updateUsers).toHaveBeenCalledWith(dispatchMock,[mockUser]);
+  });
+
+  it('should call updateNoOfApiCall with mockNoOfApiCall + 1',() =>{
+    expect(updateNoOfApiCall).toHaveBeenCalledWith(dispatchMock,mockNoOfApiCall+1);
+  });
+
 });
